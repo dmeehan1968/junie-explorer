b4fecb1950ae933688c386b753bef445ea7fc002
Dave Meehan <dave_meehan@replicated.co.uk>
2025-07-29 17:43:05 +0100

    Refactor theme switcher to improve scalability and reduce redundancy
    - Centralized theme names into a single array to improve manageability.
    - Dynamically generated theme dropdown items using `map` function.
    - Simplified HTML generation by replacing hardcoded list items with dynamic template strings.

--- src/utils/themeSwitcher.ts
+++ src/utils/themeSwitcher.ts
@@ -1,5 +1,47 @@
+// Array of capitalized theme names
+const themes = [
+  'Auto',
+  'Light',
+  'Dark',
+  'Cupcake',
+  'Bumblebee',
+  'Emerald',
+  'Corporate',
+  'Synthwave',
+  'Retro',
+  'Cyberpunk',
+  'Valentine',
+  'Halloween',
+  'Garden',
+  'Forest',
+  'Aqua',
+  'Lofi',
+  'Pastel',
+  'Fantasy',
+  'Wireframe',
+  'Black',
+  'Luxury',
+  'Dracula',
+  'CMYK',
+  'Autumn',
+  'Business',
+  'Acid',
+  'Lemonade',
+  'Night',
+  'Coffee',
+  'Winter',
+  'Dim',
+  'Nord',
+  'Sunset',
+]
+
 // Function to generate theme switcher HTML
 export function ThemeSwitcher(): string {
+  const themeListItems = themes.map(theme => {
+    const lowercaseTheme = theme.toLowerCase()
+    return `<li><a onclick="setTheme('${lowercaseTheme}')" onmouseover="previewTheme('${lowercaseTheme}')" data-testid="theme-${lowercaseTheme}">${theme}</a></li>`
+  }).join('\n        ')
+
   return `
     <div class="dropdown dropdown-end">
       <div tabindex="0" role="button" class="btn btn-ghost btn-sm" data-testid="theme-switcher">
@@ -8,39 +50,7 @@ export function ThemeSwitcher(): string {
         </svg>
       </div>
       <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow max-h-96 overflow-y-auto" onmouseleave="restoreTheme()">
-        <li><a onclick="setTheme('auto')" onmouseover="previewTheme('auto')" data-testid="theme-auto">Auto</a></li>
-        <li><a onclick="setTheme('light')" onmouseover="previewTheme('light')" data-testid="theme-light">Light</a></li>
-        <li><a onclick="setTheme('dark')" onmouseover="previewTheme('dark')" data-testid="theme-dark">Dark</a></li>
-        <li><a onclick="setTheme('cupcake')" onmouseover="previewTheme('cupcake')" data-testid="theme-cupcake">Cupcake</a></li>
-        <li><a onclick="setTheme('bumblebee')" onmouseover="previewTheme('bumblebee')" data-testid="theme-bumblebee">Bumblebee</a></li>
-        <li><a onclick="setTheme('emerald')" onmouseover="previewTheme('emerald')" data-testid="theme-emerald">Emerald</a></li>
-        <li><a onclick="setTheme('corporate')" onmouseover="previewTheme('corporate')" data-testid="theme-corporate">Corporate</a></li>
-        <li><a onclick="setTheme('synthwave')" onmouseover="previewTheme('synthwave')" data-testid="theme-synthwave">Synthwave</a></li>
-        <li><a onclick="setTheme('retro')" onmouseover="previewTheme('retro')" data-testid="theme-retro">Retro</a></li>
-        <li><a onclick="setTheme('cyberpunk')" onmouseover="previewTheme('cyberpunk')" data-testid="theme-cyberpunk">Cyberpunk</a></li>
-        <li><a onclick="setTheme('valentine')" onmouseover="previewTheme('valentine')" data-testid="theme-valentine">Valentine</a></li>
-        <li><a onclick="setTheme('halloween')" onmouseover="previewTheme('halloween')" data-testid="theme-halloween">Halloween</a></li>
-        <li><a onclick="setTheme('garden')" onmouseover="previewTheme('garden')" data-testid="theme-garden">Garden</a></li>
-        <li><a onclick="setTheme('forest')" onmouseover="previewTheme('forest')" data-testid="theme-forest">Forest</a></li>
-        <li><a onclick="setTheme('aqua')" onmouseover="previewTheme('aqua')" data-testid="theme-aqua">Aqua</a></li>
-        <li><a onclick="setTheme('lofi')" onmouseover="previewTheme('lofi')" data-testid="theme-lofi">Lofi</a></li>
-        <li><a onclick="setTheme('pastel')" onmouseover="previewTheme('pastel')" data-testid="theme-pastel">Pastel</a></li>
-        <li><a onclick="setTheme('fantasy')" onmouseover="previewTheme('fantasy')" data-testid="theme-fantasy">Fantasy</a></li>
-        <li><a onclick="setTheme('wireframe')" onmouseover="previewTheme('wireframe')" data-testid="theme-wireframe">Wireframe</a></li>
-        <li><a onclick="setTheme('black')" onmouseover="previewTheme('black')" data-testid="theme-black">Black</a></li>
-        <li><a onclick="setTheme('luxury')" onmouseover="previewTheme('luxury')" data-testid="theme-luxury">Luxury</a></li>
-        <li><a onclick="setTheme('dracula')" onmouseover="previewTheme('dracula')" data-testid="theme-dracula">Dracula</a></li>
-        <li><a onclick="setTheme('cmyk')" onmouseover="previewTheme('cmyk')" data-testid="theme-cmyk">CMYK</a></li>
-        <li><a onclick="setTheme('autumn')" onmouseover="previewTheme('autumn')" data-testid="theme-autumn">Autumn</a></li>
-        <li><a onclick="setTheme('business')" onmouseover="previewTheme('business')" data-testid="theme-business">Business</a></li>
-        <li><a onclick="setTheme('acid')" onmouseover="previewTheme('acid')" data-testid="theme-acid">Acid</a></li>
-        <li><a onclick="setTheme('lemonade')" onmouseover="previewTheme('lemonade')" data-testid="theme-lemonade">Lemonade</a></li>
-        <li><a onclick="setTheme('night')" onmouseover="previewTheme('night')" data-testid="theme-night">Night</a></li>
-        <li><a onclick="setTheme('coffee')" onmouseover="previewTheme('coffee')" data-testid="theme-coffee">Coffee</a></li>
-        <li><a onclick="setTheme('winter')" onmouseover="previewTheme('winter')" data-testid="theme-winter">Winter</a></li>
-        <li><a onclick="setTheme('dim')" onmouseover="previewTheme('dim')" data-testid="theme-dim">Dim</a></li>
-        <li><a onclick="setTheme('nord')" onmouseover="previewTheme('nord')" data-testid="theme-nord">Nord</a></li>
-        <li><a onclick="setTheme('sunset')" onmouseover="previewTheme('sunset')" data-testid="theme-sunset">Sunset</a></li>
+        ${themeListItems}
       </ul>
     </div>
   `
