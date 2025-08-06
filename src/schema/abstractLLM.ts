cd1e861b48f6dfdbe56108c4ba987c49949985e6
Dave Meehan <dave_meehan@replicated.co.uk>
2025-08-06 13:02:32 +0100

    Add `isSummarizer` boolean to `AbstractLLM` schema
    - Introduced a new `isSummarizer` field with a default value of `false` to the `AbstractLLM` schema.

--- src/schema/abstractLLM.ts
+++ src/schema/abstractLLM.ts
@@ -1,6 +1,7 @@
 import * as z from "zod"
 
 export const AbstractLLM = z.looseObject({
+  isSummarizer: z.boolean().default(() => false),
   provider: z.string(),
   name: z.string(),
   inputPrice: z.number().optional(),              // deprecated
